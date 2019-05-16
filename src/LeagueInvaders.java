d3b366431ca038b4341d666a9618ed5c6a19ec0a
League Student <league@WTS9.attlocal.net>
2019-05-16 16:58:21 -0700

    LeagueInvaders(IP)

--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,4 +1,25 @@
+import javax.swing.JFrame;
 
 public class LeagueInvaders {
+	JFrame frame;
+	final static int WIDTH = 500;
+	final static int HEIGHT = 800;
+	
+public static void main(String[] args) {
+	
+	
+}
+
+public LeagueInvaders(){
+	frame = new JFrame();
+}
+
+void setup() {
+	frame.setVisible(true);
+	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+}
+
 
 }
